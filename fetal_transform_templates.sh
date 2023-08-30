d6b8189c72142e8b539866a6da20daee2d069dbc
Pablo Prieto <pabloprieto@Pablos-MacBook-Pro.local>
2023-08-30 11:52:51 +0100

    Added Functionality to perform registration to any Group Level of choice added

--- fetal_transform_templates.sh
+++ fetal_transform_templates.sh
@@ -228,14 +228,16 @@ mkdir -p "$concatenated_folder"
 
 # These are three distinct manners in which
 concatenate_for_36() {
+    local end_week=$1
+    let "prev=end_week-1"
     # Specific method for g = 36
 
     # Copy the initial transform file to the new directory
     for side in left right; do
-        cp "$output_folder/fetal.week35_36.$side.Sulc_Wrap.sphere.reg.surf.gii" "$concatenated_folder/"
+        cp "$output_folder/fetal.week${prev}_${end_week}.$side.Sulc_Wrap.sphere.reg.surf.gii" "$concatenated_folder/"
     done
 
-    for i in {35..22}; do
+    for i in $(seq $prev -1 22); do
         for side in left right; do
             let "i_previous=i-1"
             echo "$i $i_previous"
@@ -243,15 +245,15 @@ concatenate_for_36() {
             in_sphere_file="$output_folder/fetal.week${i_previous}_${i}.$side.Sulc_Wrap.sphere.reg.surf.gii" # 34-35 , 33-34, 32-33    -- 21-22
             sphere_project_to="$atlas_folder/fetal.week${i}.$side.sphere.surf.gii"                           #    35,     34,    33    --    22  
             # Conditional check to determine the directory for sphere_unproject_to                           #    35-36,  34-36, 33-36 --    22-36
-            if [[ $i -eq 35 ]]; then                                                                        #Out:   34-36   33-36 32-36        21-36
-                sphere_unproject_to="$output_folder/fetal.week${i}_36.$side.Sulc_Wrap.sphere.reg.surf.gii"
+            if [[ $i -eq ${prev} ]]; then                                                                        #Out:   34-36   33-36 32-36        21-36
+                sphere_unproject_to="$output_folder/fetal.week${i}_${end_week}.$side.Sulc_Wrap.sphere.reg.surf.gii"
             else
-                sphere_unproject_to="$concatenated_folder/fetal.week${i}_36.$side.Sulc_Wrap.sphere.surf.gii"
+                sphere_unproject_to="$concatenated_folder/fetal.week${i}_${end_week}.$side.Sulc_Wrap.sphere.surf.gii"
             fi    
             # now it needs to be accessed the unproject to from the concatenated folder
-            sphere_out="$concatenated_folder/fetal.week${i_previous}_36.$side.Sulc_Wrap.sphere.surf.gii"  
+            sphere_out="$concatenated_folder/fetal.week${i_previous}_${end_week}.$side.Sulc_Wrap.sphere.surf.gii"  
         
-            log_verbose "Concatenating Registrations from gestational week $i $side side to gestational week 36 $side side"
+            log_verbose "Concatenating Registrations from gestational week $i $side side to gestational week ${end_week} $side side"
 
             wb_command -surface-sphere-project-unproject $in_sphere_file $sphere_project_to $sphere_unproject_to $sphere_out
         done
@@ -260,21 +262,23 @@ concatenate_for_36() {
 }
 
 concatenate_for_21() {
+    local start_week=$1
+    let "nex=start_week+1"
     # specific logic for g = 21
 
     # Copy the initial transform file to the new directory
     for side in left right; do
-        cp "$output_folder/fetal.week21_22.$side.Sulc_Wrap.sphere.reg.surf.gii" "$concatenated_folder/"
+        cp "$output_folder/fetal.week${start_week}_${nex}.$side.Sulc_Wrap.sphere.reg.surf.gii" "$concatenated_folder/"
 
-        sphere_in_inverse="$atlas_folder/fetal.week22.$side.sphere.surf.gii"  
-        project_to_inverse="$concatenated_folder/fetal.week21_22.$side.Sulc_Wrap.sphere.reg.surf.gii"
-        sphere_unproject_to_inverse="$atlas_folder/fetal.week21.$side.sphere.surf.gii"  
-        sphere_out_inverse="$concatenated_folder/fetal.week22_21.$side.Sulc_Wrap.sphere.surf.gii" 
+        sphere_in_inverse="$atlas_folder/fetal.week${nex}.$side.sphere.surf.gii"  
+        project_to_inverse="$concatenated_folder/fetal.week${start_week}_${nex}.$side.Sulc_Wrap.sphere.reg.surf.gii"
+        sphere_unproject_to_inverse="$atlas_folder/fetal.week${start_week}.$side.sphere.surf.gii"  
+        sphere_out_inverse="$concatenated_folder/fetal.week${nex}_${start_week}.$side.Sulc_Wrap.sphere.surf.gii" 
 
         wb_command -surface-sphere-project-unproject $sphere_in_inverse $project_to_inverse $sphere_unproject_to_inverse $sphere_out_inverse
     done
 
-    for i in {22..35}; do
+    for i in $(seq $nex 35); do
         for side in left right; do
             let "i_next=i+1"
             echo "$i $i_next"
@@ -282,48 +286,42 @@ concatenate_for_21() {
             sphere_unproject_to="$output_folder/fetal.week${i}_${i_next}.$side.Sulc_Wrap.sphere.reg.surf.gii" # 21-22 , 23-24, 32-33    -- 21-22
             sphere_project_to="$atlas_folder/fetal.week${i}.$side.sphere.surf.gii"                           #    22,     23,    33    --    22  
             # Conditional check to determine the directory for sphere_unproject_to                           #    22-23,  21-23, 33-36 --    22-36
-            if [[ $i -eq 22 ]]; then                                                                        #Out:   21-23   21-24 32-36        21-36
-                in_sphere_file="$output_folder/fetal.week21_${i}.$side.Sulc_Wrap.sphere.reg.surf.gii" 
+            if [[ $i -eq ${nex} ]]; then                                                                        #Out:   21-23   21-24 32-36        21-36
+                in_sphere_file="$output_folder/fetal.week${start_week}_${i}.$side.Sulc_Wrap.sphere.reg.surf.gii" 
             else
-                in_sphere_file="$concatenated_folder/fetal.week21_${i}.$side.Sulc_Wrap.sphere.surf.gii" 
+                in_sphere_file="$concatenated_folder/fetal.week${start_week}_${i}.$side.Sulc_Wrap.sphere.surf.gii" 
             fi    
             # now it needs to be accessed the unproject to from the concatenated folder
-            sphere_out="$concatenated_folder/fetal.week21_${i_next}.$side.Sulc_Wrap.sphere.surf.gii"  
+            sphere_out="$concatenated_folder/fetal.week${start_week}_${i_next}.$side.Sulc_Wrap.sphere.surf.gii"  
             echo "Group level is: $group_level"
-            log_verbose "Concatenating Registrations from gestational week 21 side to gestational week $i $side side"
+            log_verbose "Concatenating Registrations from gestational week ${start_week} side to gestational week $i $side side"
 
             wb_command -surface-sphere-project-unproject $in_sphere_file $sphere_project_to $sphere_unproject_to $sphere_out
             # inverting the transforms, we currently have 21-22... we want 22-21
             sphere_in_inverse="$atlas_folder/fetal.week${i_next}.$side.sphere.surf.gii"  
-            project_to_inverse="$concatenated_folder/fetal.week21_${i_next}.$side.Sulc_Wrap.sphere.surf.gii" 
-            sphere_unproject_to_inverse="$atlas_folder/fetal.week21.$side.sphere.surf.gii"  
-            sphere_out_inverse="$concatenated_folder/fetal.week${i_next}_21.$side.Sulc_Wrap.sphere.surf.gii" 
+            project_to_inverse="$concatenated_folder/fetal.week${start_week}_${i_next}.$side.Sulc_Wrap.sphere.surf.gii" 
+            sphere_unproject_to_inverse="$atlas_folder/fetal.week${start_week}.$side.sphere.surf.gii"  
+            sphere_out_inverse="$concatenated_folder/fetal.week${i_next}_${start_week}.$side.Sulc_Wrap.sphere.surf.gii" 
             wb_command -surface-sphere-project-unproject $sphere_in_inverse $project_to_inverse $sphere_unproject_to_inverse $sphere_out_inverse
         done
     done
-
 }
 
-#default_concatenate() {
-    # Default method for group_level other than 21 and 36
-    #for i in $(seq $group_level -1 22); do
-        # Implement logic using $i as the week index
-        # (you can use a similar approach to what you've done for g=36,
-        # but adjust accordingly for any week other than 36)
-    #done
-#}
-
-# Call the appropriate function based on group_level:
+default_concatenate(){
+    local group_level=$1
+    concatenate_for_36 $group_level
+    concatenate_for_21 $group_level
+}
 
 case "$group_level" in
     36)
-        concatenate_for_36
+        concatenate_for_36 36
         ;;
     21)
-        concatenate_for_21
+        concatenate_for_21 21
         ;;
     *)
-        #default_concatenate
+        default_concatenate $group_level
         ;;
 esac
 
@@ -339,22 +337,24 @@ log_verbose "Creating reprojected_metrics directory, where resampled metrics wil
 mkdir -p "$reprojected_metrics"
 
 reproject_for_36(){
+    local end_week=$1
+    let "prev=end_week-1"
     # Copy the initial transform file to the new directory
     for side in left right; do
-        cp "$output_folder/fetal.week35_36.$side.Sulc_Wrap.transformed_and_reprojected.func.gii" "$reprojected_metrics/"
+        cp "$output_folder/fetal.week${prev}_${end_week}.$side.Sulc_Wrap.transformed_and_reprojected.func.gii" "$reprojected_metrics/"
     done
 
-    for i in {35..21}; do
+    for i in $(seq $prev -1 21); do
         for side in left right; do
 
             in_metric="$atlas_folder/fetal.week${i}.$side.sulc.shape.gii"              
-            sphere_out="$concatenated_folder/fetal.week${i}_36.$side.Sulc_Wrap.sphere.surf.gii"
-            new_sphere="$atlas_folder/fetal.week36.$side.sphere.surf.gii"
+            sphere_out="$concatenated_folder/fetal.week${i}_${end_week}.$side.Sulc_Wrap.sphere.surf.gii"
+            new_sphere="$atlas_folder/fetal.week${end_week}.$side.sphere.surf.gii"
             current_area="$atlas_folder/fetal.week${i}.$side.midthickness.surf.gii"
-            new_area="$atlas_folder/fetal.week36.$side.midthickness.surf.gii"
-            out_metric="$reprojected_metrics/fetal.week${i}_36.$side.Sulc_Wrap.transformed_and_reprojected.shape.gii"
+            new_area="$atlas_folder/fetal.week${end_week}.$side.midthickness.surf.gii"
+            out_metric="$reprojected_metrics/fetal.week${i}_${end_week}.$side.Sulc_Wrap.transformed_and_reprojected.shape.gii"
             
-            log_verbose "Resampling metrics from gestational week $i to sphere $next_week"
+            log_verbose "Resampling metrics from gestational week $i to sphere $end_week"
             
             wb_command -metric-resample $in_metric $sphere_out $new_sphere ADAP_BARY_AREA -area-surfs $current_area $new_area $out_metric
         done
@@ -363,33 +363,41 @@ reproject_for_36(){
 }
 
 reproject_for_21(){
-
-    for i in {22..36}; do
+    local start_week=$1
+    let "nex=start_week+1"
+    
+    for i in $(seq $nex 36); do
         for side in left right; do
 
             in_metric="$atlas_folder/fetal.week${i}.$side.sulc.shape.gii"              
-            sphere_out="$concatenated_folder/fetal.week${i}_21.$side.Sulc_Wrap.sphere.surf.gii"
-            new_sphere="$atlas_folder/fetal.week21.$side.sphere.surf.gii"
+            sphere_out="$concatenated_folder/fetal.week${i}_${start_week}.$side.Sulc_Wrap.sphere.surf.gii"
+            new_sphere="$atlas_folder/fetal.week${start_week}.$side.sphere.surf.gii"
             current_area="$atlas_folder/fetal.week${i}.$side.midthickness.surf.gii"
-            new_area="$atlas_folder/fetal.week21.$side.midthickness.surf.gii"
-            out_metric="$reprojected_metrics/fetal.week${i}_21.$side.Sulc_Wrap.transformed_and_reprojected.shape.gii"
+            new_area="$atlas_folder/fetal.week${start_week}.$side.midthickness.surf.gii"
+            out_metric="$reprojected_metrics/fetal.week${i}_${start_week}.$side.Sulc_Wrap.transformed_and_reprojected.shape.gii"
             
-            log_verbose "Resampling metrics from gestational week $i to sphere $next_week"
+            log_verbose "Resampling metrics from gestational week $i to sphere $start_week"
             
             wb_command -metric-resample $in_metric $sphere_out $new_sphere ADAP_BARY_AREA -area-surfs $current_area $new_area $out_metric
         done
     done
 }
 
+default_reproject(){
+    local group_level=$1
+    reproject_for_21 $group_level
+    reproject_for_36 $group_level
+}
+
 case "$group_level" in
     36)
-        reproject_for_36
+        reproject_for_36 36
         ;;
     21)
-        reproject_for_21
+        reproject_for_21 21
         ;;
     *)
-        #default_reproject
+        default_reproject $group_level
         ;;
 esac
 
